c3e7a39512f1f1d2de5a3e4d4cd58f2bf45c5ada
Vinny <quinterovinny@gmail.com>
2021-08-07 01:07:56 -0400

    Fix poisition of menu, Render item sprites onto said menu

--- main.lua
+++ main.lua
@@ -12,21 +12,43 @@ local NUM_ITEMS = Isaac.GetItemConfig():GetCollectibles().Size - 1
 -- Store collected items in table, if size changes between frames, recheck items?
 -- Ensure not to insert duplicate items when rechecking items
 
+-- Pills/Cards/Trinkets not currently supported
 -- (LAST) Remove lost items from item pool
+-- Shader to darken screen slightly when opening menu?
+
+-- Table holding ID of every item owned
+local collectedItemIDs = {}
+-- Table holding sprite of every item owned
+local collectedItemSprites = {}
 
 local menuOpen = false
 local itemMenu = Sprite()
-
--- Table holding ID of every item owned
-local collectedItems = {}
-
-local menuPos = Vector(70,85)
-
-local str = 'Hello'
-local extra = ''
-local str2 = ''
-
 itemMenu:Load("gfx/ui/itemmenu.anm2", true)
+
+local itemMenuAttrs = {
+    -- Where to create the menu
+    pos = Vector(175, 240),
+    scale = Vector(0.7, 0.7),
+
+    -- Where to begin drawing items ON the menu
+    origin = Vector(180, 245),
+    spacing = Vector(25, 25),
+
+    -- Number of columns and rows to display items in 
+    layout = Vector(5, 4)
+}
+
+local itemMenuIconAttrs = {}
+
+local itemDescAttrs = {
+    pos = Vector(325, 60)
+}
+
+-- Debug strings
+local str = ''
+local str2 = ''
+local str3 = ''
+
 
 local function initMenu()
     itemMenu:SetFrame("Idle", 0)
@@ -80,23 +102,29 @@ local function showPlayerItems()
     end
 end
 
+-- TODO: This will likely become an onEvent function once finalized
+-- Update collectedItemIDs list with ID of every currently held collectible
+local function heldCollectibles()
+    local player = Isaac.GetPlayer(0)
+
+    for i=1, NUM_ITEMS do
+        if player:HasCollectible(i) then
+            local item = Isaac.GetItemConfig():GetCollectible(i)
+
+            if not contains(collectedItemIDs, item.ID) then
+                table.insert(collectedItemIDs, item.ID)
+                table.insert(collectedItemSprites, Sprite())
+            end
+        end
+    end
+end
+
 function mod:onRender()
+    local player = Isaac.GetPlayer(0)
+
     if Input.IsButtonTriggered(Keyboard.KEY_J, 0) and not Game():IsPaused() then
         menuOpen = not menuOpen
     end
-
-    if Input.IsButtonTriggered(Keyboard.KEY_L, 0) then
-        savePlayerItems()
-    end
-
-    -- if Input.IsButtonTriggered(Keyboard.KEY_N, 0) then
-    --     local spry = Sprite()
-    --     local ent = Isaac.GetItemConfig():GetCollectible(1) -- Sad onion
-    --     spry:Load("gfx/ui/menuitem.anm2", true)
-    --     spry:ReplaceSpriteSheet(0, ent.GfxFileName)
-    --     spry:LoadGraphics()
-    --     -- spry:Render(Vector(100,100), Vector(0,0), Vector(500,500))
-    -- end
 
     if menuOpen then
         -- Close menu
@@ -104,42 +132,28 @@ function mod:onRender()
             menuOpen = false
         end
 
-        local player = Isaac.GetPlayer(0)
-
-        local stuff = {}
-
-        for i=1, AfterbirthPlusItems do
-            if player:HasCollectible(i) then
-                table.insert(collectedItems, Isaac.GetItemConfig():GetCollectible(i))
-                Isaac.DebugString('Isaac has item - '..collectedItems[i].Name)
-            end
-        end
+        heldCollectibles()
         
-        for i=1, #collectedItems do
-            local tmp = Sprite()
-            tmp:Load("gfx/ui/menuitem.anm2", true)
-            table.insert(stuff, tmp)
-        end
-
-        for i=1, #collectedItems do
-            -- Isaac.DebugString(collectedItems[i].GfxFileName)
-            Isaac.DebugString('STUFF BREAKDOWN- ')
-            for j=1, #stuff do
-                Isaac.DebugString('Stuff '..j..' - '..tostring(stuff[j]))
-            end
-            Isaac.DebugString('ITEMS BREAKDOWN - '..type(collectedItems[i]))
-            for j=1, #collectedItems do
-                Isaac.DebugString('Stuff '..j..' - '..tostring(collectedItems[j]))
-            end
-            -- -- Must load first, otherwise there is no spritesheet to replace
-            stuff[i]:ReplaceSpriteSheet(0, collectedItems[i].GfxFileName)
-            stuff[i]:LoadGraphics()
-            -- stuff[i].Render(Vector(100,100))
-        end
-
-
+        -- Create menu to load items on top of
+        itemMenu.Scale = itemMenuAttrs.scale
         itemMenu:SetFrame("Idle", 0)
-        itemMenu:RenderLayer(0, Isaac.WorldToRenderPosition(menuPos))
+        itemMenu:RenderLayer(0, Isaac.WorldToRenderPosition(itemMenuAttrs.pos))
+
+        -- Does not yet support item rows. Will just render all items in a single row infinitely
+        if next(collectedItemIDs) then
+            local offset
+            for i=1, #collectedItemIDs do
+                collectedItemSprites[i]:Load("gfx/ui/menuitem.anm2", true)
+                collectedItemSprites[i]:ReplaceSpritesheet(0, Isaac.GetItemConfig():GetCollectible(collectedItemIDs[i]).GfxFileName)
+                collectedItemSprites[i]:LoadGraphics()
+                collectedItemSprites[i]:SetFrame("Idle", 0)
+
+                offset = Vector(itemMenuAttrs.spacing.X * i, 0)
+
+                collectedItemSprites[i]:RenderLayer(0, Isaac.WorldToRenderPosition(itemMenuAttrs.origin + offset))
+            end
+        end
+
 
         -- UI movment logic goes here
         if not Game():IsPaused() then
@@ -161,10 +175,9 @@ function mod:onInput(entity, hook, button)
 end
 
 function mod:debugText()
-    extra = tostring(menuOpen)
     Isaac.RenderText(str, 100, 100, 255, 0, 0, 255)
-    Isaac.RenderText(extra, 100, 125, 0, 255, 0, 255)
-    Isaac.RenderText(str2, 100, 150, 0, 0, 255, 255)
+    Isaac.RenderText(str2, 100, 125, 0, 255, 0, 255)
+    Isaac.RenderText(str3, 100, 150, 0, 0, 255, 255)
 end
 
 
@@ -174,3 +187,11 @@ mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.onRender)
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.debugText)
 
 mod:AddCallback(ModCallbacks.MC_INPUT_ACTION, mod.onInput)
+
+-- When to refresh collectedItems?
+--   POST_UPDATE/POST_PLAYER_UPDATE/NEW_ROOM/EVAL_CACHE
+--      POST_UPDATE called 30x per second, not called when paused
+--      POST_PLAYER_UPDATE called 60x per second, not called when paused
+--      NEW_ROOM should determine if it works for ALL rooms or only on first entry
+
+-- POST_PICKUP_INIT is useful if we can detect items on the floor somehow
